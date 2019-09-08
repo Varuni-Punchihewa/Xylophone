c10bab740abbee753c9d981a381c69364b4cfa3d
Varuni-Punchihewa <1995varuni@gmail.com>
2019-09-09 02:20:38 +0530

    add sounds to the xylophone

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
-class XylophoneApp extends StatelessWidget {
+class XylophoneApp extends StatefulWidget {
+  @override
+  _XylophoneAppState createState() => _XylophoneAppState();
+}
+
+class _XylophoneAppState extends State<XylophoneApp> {
+  AudioCache player = AudioCache();
+  Expanded buildKey(Color c, int n) {
+    return Expanded(
+      child: FlatButton(
+        color: c,
+        onPressed: () {
+          player.play('note$n.wav');
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                buildKey(Colors.red, 1),
+                buildKey(Colors.orange, 2),
+                buildKey(Colors.yellow, 3),
+                buildKey(Colors.green, 4),
+                buildKey(Colors.blue, 5),
+                buildKey(Colors.indigo, 6),
+                buildKey(Colors.purple, 7),
+              ],
+            ),
+          ),
         ),
       ),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
